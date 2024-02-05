6cdace444e588dcccccd0e3d6a86fd974c2eab9e
levirevebs <beverlev001@warren.k12.in.us>
2024-02-05 08:01:13 -0500

    123

---
+++ 01_Type Annotations Assn.py
@@ -0,0 +1,6 @@
+def average(a: int, b: int) -> int:
+    return (a + b) / 2
+
+number1 : int = float(input("Enter a decimal number: "))
+number2 : int = float(input("Enter another decimal number: "))
+print(average(number1, number2))
