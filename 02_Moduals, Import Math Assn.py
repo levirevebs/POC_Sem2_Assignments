f590d52437a1ed973af47f235be4bf8484da1cd3
levirevebs <beverlev001@warren.k12.in.us>
2024-02-06 08:03:18 -0500

    hypot

---
+++ 02_Moduals, Import Math Assn.py
@@ -0,0 +1,9 @@
+import math
+
+leg1 = float(input("Give me a number with a decimal: "))
+leg2 = float(input("Give me another number with a decimal: "))
+
+hyp = (math.hypot(leg1, leg2))
+
+print("The hypotenuse is", hyp)
+
---
+++ 02_Moduels, Math Import.py
@@ -0,0 +1,85 @@
+# # from sys import *
+# # from sys import exit
+# # import sys
+
+# # import sys
+# # import math
+# # import sys
+# # import math
+# # import sys
+
+# # for name in dir(sys):
+# #     print(name, end='\t')
+# # sys.exit()
+
+# # sys.exit324()
+
+
+# # def exit():
+# #     print('I wanna exit')
+
+
+# # exit()
+# # sys.exit()
+
+# # exit()
+
+
+# # def exit():
+# #     print('I wanna exit')
+
+
+# # exit()
+# # from sys import *
+# from sys import exit as bye_bye
+# import sys  # you need to add sys.
+# from sys import *
+# import sys as s
+
+# s.exit()
+
+# bye_bye()
+
+import math
+
+for name in dir(math):
+    print(name, end='\t')
+    
+# ceil(), floor(), trunc()
+print(math.ceil(3.6))
+print(math.floor(3.6))
+print(math.trunc(3.6))
+
+# ceil()
+print(math.ceil(3.1))
+print(math.ceil(3.505))
+print(math.ceil(3.945))
+print(math.ceil(3.0))
+print(math.ceil(-5.4))
+
+# floor()
+print(math.floor(3.1))
+print(math.floor(3.505))
+print(math.floor(3.945))
+print(math.floor(3.0))
+print(math.floor(-5.4))
+
+# trunc()
+print(math.trunc(3.1))
+print(math.trunc(3.505))
+print(math.trunc(3.945))
+print(math.trunc(3.0))
+print(math.trunc(-5.4))
+
+# factorial()
+# 3! = 3 * 2 * 1 = 6
+# 4! = 4 * 3 * 2 * 1 = 24
+print(math.factorial(3))
+print(math.factorial(4))
+
+# sqrt()
+print(math.sqrt(16))
+print(math.sqrt(100))
+
+# hypot()
+print(math.hypot(3, 4))
