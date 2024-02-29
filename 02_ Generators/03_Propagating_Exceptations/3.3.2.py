e29337a8cd088f79572027d1f13764160c090b7f
levirevebs <beverlev001@warren.k12.in.us>
2024-02-29 07:59:13 -0500

    yehaw

--- 02_ Generators/03_Propagating_Exceptations/3.3.2.py
+++ 02_ Generators/03_Propagating_Exceptations/3.3.2.py
@@ -1,11 +1,8 @@
-number1 = input
-number2 = input
 try:
-    input("Give me a number: ")
-    input("Give me another number: ")
+    number1 = input("Give me a number: ")
+    number2 = input("Give me another number: ")
 except ValueError:
     print("ValueError: No Integer was given.")
-
 try:
     number1 / number2
 except ZeroDivisionError:
---
+++ 07_OOP/Notes/notes (making an object).py
@@ -0,0 +1,18 @@
+# making an object.
+
+'''
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def get_area(self):
+        return self.width * self.height
+
+
+a = int(input("Num: "))
+b = int(input("Num: "))
+
+rectangle = Rectangle(a, b)
+print('The area is', rectangle.get_area) # "get_area" is a method.
+'''
